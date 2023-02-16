0d476023a65b23f265c4cf2a581783a36a827f8b
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-02-16 01:04:20 -0600

    initial commit

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,12 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HelperFunctions {
+  static String userLoggedInKey = 'LOGGEDINKEY';
+  static String userNameKey = 'USERNAMEKEY';
+  static String userEmailInKey = 'USEREMAILKEY';
+
+  static Future<bool?> getUserLoggedInStatus() async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return sf.getBool(userLoggedInKey);
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String apiKey = 'AIzaSyAuMbmK1Pn8Hj5EytzLuHO5AH1FYX74o6g';
+  static String appId = '1:169317373033:web:4cf1e0ffa1a0a0536ffef8';
+  static String messagingSenderId = '169317373033';
+  static String projectId = 'chattingapp-d9851';
+}
