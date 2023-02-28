940a8624fc8bf4d5ce3a9bc7618f57ca9a9bfa80
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-02-28 17:07:12 -0600

    sharedpreference to save and retrieve data

---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,178 @@
+import 'package:chatting/helper/helper_function.dart';
+import 'package:chatting/pages/home_page.dart';
+import 'package:chatting/service/auth_service.dart';
+import 'package:chatting/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool _isLoading = false;
+  final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  String fullName = '';
+  AuthService authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Chatting',
+                        style: GoogleFonts.josefinSans(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/logo.png'),
+                        )),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          hintText: 'Full Name',
+                        ),
+                        onChanged: (value) => setState(
+                          () {
+                            fullName = value;
+                          },
+                        ),
+                        validator: (value) {
+                          if (value!.isNotEmpty) {
+                            return null;
+                          } else {
+                            return 'Name cannot be empty';
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'Email',
+                        ),
+                        onChanged: (value) => setState(
+                          () {
+                            email = value;
+                          },
+                        ),
+                        validator: (val) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(val!)
+                              ? null
+                              : "Please enter a valid email";
+                        },
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: 'Password',
+                        ),
+                        onChanged: (value) => setState(
+                          () {
+                            password = value;
+                          },
+                        ),
+                        validator: (value) {
+                          if (value!.length < 6) {
+                            return 'Password must be at least 6 characters';
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          register();
+                        },
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text.rich(
+                        TextSpan(
+                          text: 'Already have an account? ',
+                          children: [
+                            TextSpan(
+                                text: 'Login here',
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const LoginPage());
+                                  }),
+                          ],
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+
+  register() async {
+    if (formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      await authService
+          .registerUserWithEmailAndPassword(fullName, email, password)
+          .then((value) async {
+        if (value == true) {
+          await HelperFunctions.saveUserLoggedInStatus(true);
+          await HelperFunctions.saveUserEmailSF(email);
+          await HelperFunctions.saveUserNameSF(fullName);
+          nextScreenReplace(context, const HomePage());
+        } else {
+          showSnackbar(context, Colors.red, value);
+          _isLoading = false;
+        }
+      });
+    }
+  }
+}
