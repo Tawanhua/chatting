16430f1db0e10dcb0c52b8177ab3f5902b024b6d
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-02-28 17:22:04 -0600

    created search page

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('SearchPage')),
+    );
+  }
+}
