890740422744922b27df1f875e9c6253ff8a7c9e
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-03-03 00:13:06 -0600

    listview for groups

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:chatting/pages/profile_page.dart';
 import 'package:chatting/pages/search_page.dart';
 import 'package:chatting/service/auth_service.dart';
 import 'package:chatting/service/database_service.dart';
+import 'package:chatting/widgets/group_tile.dart';
 import 'package:chatting/widgets/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   String email = '';
   AuthService authService = AuthService();
   Stream? groups;
-  final bool _isLoading = false;
+  bool _isLoading = false;
   String groupName = '';
 
   @override
@@ -28,6 +29,14 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
     gettingUserData();
+  }
+
+  String getId(String res) {
+    return res.substring(0, res.indexOf('_'));
+  }
+
+  String getName(String res) {
+    return res.substring(res.indexOf('_') + 1);
   }
 
   gettingUserData() async {
@@ -194,7 +203,16 @@ class _HomePageState extends State<HomePage> {
         if (snapshot.hasData) {
           if (snapshot.data['groups'] != null) {
             if (snapshot.data['groups'].length != 0) {
-              return const Text('Hello');
+              return ListView.builder(
+                itemCount: snapshot.data['groups'].length,
+                itemBuilder: (context, index) {
+                  int reverseIndex = snapshot.data['groups'].length - index - 1;
+                  return GroupTile(
+                      groupId: getId(snapshot.data['groups'][reverseIndex]),
+                      groupName: getName(snapshot.data['groups'][reverseIndex]),
+                      userName: snapshot.data['fullName']);
+                },
+              );
             } else {
               return noGroupWidget();
             }
@@ -215,45 +233,67 @@ class _HomePageState extends State<HomePage> {
         barrierDismissible: false,
         context: context,
         builder: (context) {
-          return AlertDialog(
-            title: const Text(
-              'Create a group',
-              textAlign: TextAlign.left,
-            ),
-            content: Column(mainAxisSize: MainAxisSize.min, children: [
-              _isLoading == true
-                  ? const Center(child: CircularProgressIndicator())
-                  : TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          groupName = value;
-                        });
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return AlertDialog(
+                title: const Text(
+                  'Create a group',
+                  textAlign: TextAlign.left,
+                ),
+                content: Column(mainAxisSize: MainAxisSize.min, children: [
+                  _isLoading == true
+                      ? const Center(child: CircularProgressIndicator())
+                      : TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              groupName = value;
+                            });
+                          },
+                          style: const TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.red),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                ]),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
                       },
-                      style: const TextStyle(color: Colors.black),
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.red),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-            ]),
-            actions: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Cancel')),
-              ElevatedButton(onPressed: () {}, child: const Text('Create'))
-            ],
+                      child: const Text('Cancel')),
+                  ElevatedButton(
+                      onPressed: () async {
+                        if (groupName != '') {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          DatabaseService(
+                                  uid: FirebaseAuth.instance.currentUser!.uid)
+                              .createGroup(
+                                  userName,
+                                  FirebaseAuth.instance.currentUser!.uid,
+                                  groupName)
+                              .whenComplete(() => _isLoading = false);
+                          Navigator.of(context).pop();
+                          showSnackbar(context, Colors.green,
+                              'Group created successfully.');
+                        }
+                      },
+                      child: const Text('Create'))
+                ],
+              );
+            },
           );
         });
   }
