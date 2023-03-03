a615463c119f187519bf0f1a47b2ad61e73d6894
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-03-03 00:22:43 -0600

    listview widget for groups

---
+++ lib/widgets/group_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class GroupTile extends StatefulWidget {
+  final String userName;
+  final String groupId;
+  final String groupName;
+  const GroupTile(
+      {Key? key,
+      required this.groupId,
+      required this.groupName,
+      required this.userName})
+      : super(key: key);
+
+  @override
+  State<GroupTile> createState() => _GroupTileState();
+}
+
+class _GroupTileState extends State<GroupTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Text(
+            widget.groupName.substring(0, 1).toUpperCase(),
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        title: Text(
+          widget.groupName,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          'Join the conversation as ${widget.userName}',
+          style: const TextStyle(fontSize: 13),
+        ),
+      ),
+    );
+  }
+}
