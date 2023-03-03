2102c239128fab733510cb5f4a88a6bb00836f2f
Jerry Sean Huynh <sjhuynh500@gmail.com>
2023-03-03 00:21:49 -0600

    group collection, update members, assign groupid and groupname

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -8,7 +8,7 @@ class DatabaseService {
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('users');
   final CollectionReference groupCollection =
-      FirebaseFirestore.instance.collection('users');
+      FirebaseFirestore.instance.collection('groups');
 
   // Save user data
   Future savingUserData(String fullName, String email) async {
@@ -31,5 +31,30 @@ class DatabaseService {
   // Get user group
   Future getUserGroups() async {
     return userCollection.doc(uid).snapshots();
+  }
+
+  // Create group
+  Future createGroup(String userName, String id, String groupName) async {
+    DocumentReference groupDocumentReference = await groupCollection.add({
+      'groupName': groupName,
+      'groupIcon': '',
+      'admin': '${id}_$userName',
+      'members': [],
+      'groupId': '',
+      'recentMessage': '',
+      'recentMessageSender': '',
+    });
+
+    // Update members
+    await groupDocumentReference.update({
+      'members': FieldValue.arrayUnion((['${uid}_$userName'])),
+      'groupId': groupDocumentReference.id,
+    });
+
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    return await userDocumentReference.update({
+      'groups':
+          FieldValue.arrayUnion(['${groupDocumentReference.id}_$groupName'])
+    });
   }
 }
